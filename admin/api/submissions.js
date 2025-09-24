23e3de2eb2570be62d0ab8a977fb09bca1921b42
MrFarrukhT <webmfarrukh@gmail.com>
2025-09-24 20:46:21 +0500

    fix: Change import syntax for pg and update module export in submissions.js refactor: Remove type module from package.json

--- admin/api/submissions.js
+++ admin/api/submissions.js
@@ -1,4 +1,4 @@
-import { Pool } from 'pg';
+const { Pool } = require('pg');
 
 // Database connection using Neon PostgreSQL
 const pool = new Pool({
@@ -39,7 +39,7 @@ async function initializeDatabase() {
   }
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
